49750f60ff4fe6eaae36a45fe5cbf6f773dd8bfe
Łukasz Czapraga <lczapraga@gmail.com>
2020-09-06 23:57:12 +0200

    Version 0.0.2 - created Customer class, CustomerDao, implemented code for list retrieval.

--- web-customer-tracker/src/com/lucas/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/com/lucas/springdemo/controller/CustomerController.java
@@ -1,15 +1,29 @@
 package com.lucas.springdemo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.lucas.springdemo.dao.CustomerDAO;
+import com.lucas.springdemo.entity.Customer;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 	
+	@Autowired
+	private CustomerDAO customerDao;
+	
 	@RequestMapping("/list")
 	public String listCustomers(Model theModel) {
+		
+		List<Customer> theCustomers = customerDao.getCustomers();
+		
+		theModel.addAttribute("customers", theCustomers);
+		
 		return "list-customers";
 	}
 }
